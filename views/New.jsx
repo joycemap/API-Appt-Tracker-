58890e2a4d953c3f51d79fbd237ac4b3cfd912ed
joycemap <57893220+joycemap@users.noreply.github.com>
2020-02-05 09:49:44 +0800

    Added more labels

--- views/New.jsx
+++ views/New.jsx
@@ -13,22 +13,28 @@ class New extends React.Component {
 
         <form action="/appt" method="POST">
           <div style={{ paddingBottom: `10px` }}>
+            <label for="datefield"></label>
             <input type="date" placeholder="date" id="datefield" name="date" />
           </div>
 
           <div style={{ paddingBottom: `10px` }}>
+            <label for="time"></label>
             <input type="time" placeholder="time" name="time" />
           </div>
           <div style={{ paddingBottom: `10px` }}>
+            <label for="location"></label>
             <input type="text" placeholder="location" name="location" />
           </div>
           <div style={{ paddingBottom: `10px` }}>
+            <label for="doctor"></label>
             <input type="text" placeholder="doctor" name="doctor" />
           </div>
           <div style={{ paddingBottom: `10px` }}>
+            <label for="notes"></label>
             <input type="text" placeholder="notes" name="notes" />
           </div>
           <div>
+            <label for="create button"></label>
             <input
               class="btn btn-danger btn-lg"
               type="submit"
